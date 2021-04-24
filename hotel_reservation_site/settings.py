9c62b52b765c1017b9ced7f46ee215721e1a2294
Farooque Ahmed <62591655+zizopixels@users.noreply.github.com>
2021-04-24 11:41:29 -0700

    Update settings.py

--- hotel_reservation_site/settings.py
+++ hotel_reservation_site/settings.py
@@ -199,11 +199,11 @@ STATIC_URL = '/static/'
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 os.path.join(BASE_DIR, 'build/static'),
     
-)
+]
 
 
 
