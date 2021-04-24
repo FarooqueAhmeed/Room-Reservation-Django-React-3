7c335f6efb1bc6e8c0172f6de41ffde2a538c1af
zizopixels <zizopixels11@gmail.com>
2021-04-24 08:47:48 -0700

    R

--- hotel_reservation_site/settings.py
+++ hotel_reservation_site/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'SECRET_KEY'
 DEBUG =  True
 # config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','rooms-reservations.herokuapp.com']
 
 
 # Application definition
