2aba5ade055e47ae6aaf3dba7ec774efe3ebb590
zizopixels <zizopixels11@gmail.com>
2021-04-24 10:20:26 -0700

    R

---
+++ build/precache-manifest.89f1ebb9a04a3a756fa2cb726cb6e8aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3d160fa192d122c627b6d013527bf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "854600540f5f3651cee7",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "e15838805ff3ee0134ea",
+    "url": "/static/css/main.00838bf0.chunk.css"
+  },
+  {
+    "revision": "854600540f5f3651cee7",
+    "url": "/static/js/2.05a5495f.chunk.js"
+  },
+  {
+    "revision": "56de92bb6c516076b33ef30f58cd9435",
+    "url": "/static/js/2.05a5495f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15838805ff3ee0134ea",
+    "url": "/static/js/main.f2ebe903.chunk.js"
+  },
+  {
+    "revision": "05b352c51f5fa0e62d43",
+    "url": "/static/js/runtime-main.1cac40a6.js"
+  },
+  {
+    "revision": "5bc723f8545ce7c528cf370c5aa54f67",
+    "url": "/static/media/loading.5bc723f8.gif"
+  }
+]);
